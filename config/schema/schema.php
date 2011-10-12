a12513b983a57cfcb1bc0ba6f3d9dd377018d45c
bmilesp <bmilesp@gmail.com>
2011-10-11 20:15:38 -0400

    added schema, data and readme.md

--- config/schema/schema.php
+++ config/schema/schema.php
@@ -1,7 +1,7 @@
 <?php 
-/* States schema generated on: 2011-09-21 11:57:37 : 1316620657*/
-class StatesSchema extends CakeSchema {
-	var $name = 'States';
+/* states schema generated on: 2011-10-11 20:10:11 : 1318378211*/
+class statesSchema extends CakeSchema {
+	var $name = 'states';
 
 	function before($event = array()) {
 		return true;
@@ -11,9 +11,9 @@ class StatesSchema extends CakeSchema {
 	}
 
 	var $states = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'country_id' => array('type' => 'string', 'null' => true, 'default' => 'US', 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 3, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'country_id' => array('type' => 'string', 'null' => false, 'default' => 'US', 'length' => 10, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'state_name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 30, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'state' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
---
+++ readme.md
@@ -0,0 +1,5 @@
+## CakePHP States Plugin ##
+
+The States plugin is a building-block plugin that should be the de-facto standard for state data. Currently the states plugin is United States only If you want to add other countries, I would suggest branching this plugin and adding the additional countries/states to that specific branch.
+
+There is a sql dump in the config/schema folder that contains the state data. Execute this dump after you run the schema file.
